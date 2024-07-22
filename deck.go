320f6ef1a5abed73f8352837d6cfa301373a969f
Patrick Goulding <patrickgoulding@gmail.com>
2024-07-22 11:19:01 -0700

    created new deck func

--- deck.go
+++ deck.go
@@ -1,3 +1,28 @@
 package main
 
+import "fmt"
+
 type deck []string
+
+//d is reference to 'self'
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	// _ specifiies an unused variable
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+
+}
+
+func (d deck) print() {
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+// newDeck -> Creates and Returns a list of playing cards
+// print -> log the contents of a deck of cards
+// shuffle -> shuffles all the cards in a deck
+// deal -> create a hand of cards
+// saveToFile -> save a list of cards to a file on the local machine
+// newDeckFromFile -> Load a list of cards from the local machine.
 
 func main() {
-	cards := deck{newCard(), newCard()}
+	cards := newDeck()
 	cards = append(cards, "Six of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
-
-func newCard() string {
-
-	return "Five of Diamonds"
+	cards.print()
 }
