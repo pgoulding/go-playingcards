eb2f8c546d0c0227f02a5fbb1a6e85d43edb9d86
Patrick Goulding <patrickgoulding@gmail.com>
2024-07-26 10:22:19 -0700

    Finish adding cards values

--- deck.go
+++ deck.go
@@ -15,7 +15,7 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 	// _ specifiies an unused variable
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
